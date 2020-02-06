0df7a116b6dedad8e1a23a4dbe29c5d4e4c27293
yuyang-tang <60663110+yuyang-tang@users.noreply.github.com>
2020-02-06 09:40:58 -0500

    initial commit
    initial upload of files

---
+++ PHY480_homework/summing_up_down.cpp
@@ -0,0 +1,18 @@
+//  file: summing_up_down.cpp 
+//                
+//  Programmer:  Yuyang Tang yuyangta@msu.edu
+// 
+//  Revision history: 
+//  04-Feb-2020 original version
+//
+//*******************************************************************
+//include files
+#include <iostream> 
+#include <iomanip> 
+#include <fstream> 
+#include <cmath> 
+using namespace std;
+
+int main()
+{
+  
