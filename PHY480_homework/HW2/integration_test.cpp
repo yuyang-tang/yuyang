c885f06ef0a71648af3debb90f8e2f437c9659e0
yuyang-tang <60663110+yuyang-tang@users.noreply.github.com>
2020-02-28 16:01:35 -0500

    add some notes and finish homework 4 and 5

--- PHY480_homework/HW2/integration_test.cpp
+++ PHY480_homework/HW2/integration_test.cpp
@@ -4,12 +4,22 @@
 // 
 //  Revision history: 
 //      02/26/2020 original version
+//      02/28/2020 add some notes and finish homework 4 and 5
+//
 //
 //homework 2:
 //Simpson's rule:using f(x)=10**(a*log10(x)+b) to fit the data,then find a=-3.78138,b=1.86655.It looks like a line in logscale.
 //Milne rule:using f(x)=10**(a*log10(x)+b) to fit the data,then find a=-5.15001,b=3.38446.It looks like a line in logscale.
+//
 //homework 3:
 //In the plot we see that by the increase of x(max_intervals),the error will be smaller.In notes,it says that e_total=N**(-beta)+sqrt(N)e_m,so when N get larger,e_total will get smaller.They actually get the same conclusion.so we can get the max N using for Milne rule(as it possible),it will get the best answer.
+//
+//homework 4:
+//I used the three methods to calculate the integration of 1./((1.+x)*sqrt(x)) from 0 to 2.It turns out that with Simpson's rule and Milne rule the result is inf,but with GSL function it gets the same answer with the handout(1.9106).I think it is because that using Simpson's rule or Milne rule,it will calculate sum of (f(x)*h),while h is a limited value.But when x get very close to 0,f(x) will be approach to inf,so f(x)*h will be approach to inf,it can't get the correct answer.However,using the function of GSL will not get into this troble.
+//
+//homework 5:
+//For Simpson's rule,I add some codes to calculate the integration of 2N,then get the plot of (A(N)-A(2N))/A(2N).From the note,I set f(x)=a*x**b to fit the plot,and get a=0.138764,b=-0.956696.I get the result that beta=-0.956656,so the approxiation error should be alpha/N^(0.956696) (We can not know the value of alpha because we don't know the A_exact.)
+//
 //************************************************************************
 // include files 
 #include <iostream> 
@@ -26,8 +36,8 @@ int main()
   gsl_integration_workspace *work_ptr = gsl_integration_workspace_alloc(1000);
   //set up the integration variables
   const int max_intervals = 501;
-  const double a = 10;              //upper limit of integration
-  const double b = 1;               //lower limit of integration
+  const double a = 10;//2;              //upper limit of integration
+  const double b = 1;//0;               //lower limit of integration
   double abs_error= 1.0e-8;         //to avoid round-off problems
   double rel_error= 1.0e-8;         //the result will usually be much better
   double result = 0;                //set up initial value of result
@@ -39,13 +49,14 @@ int main()
   My_function.params = params_ptr;
   //open the output file stream
   ofstream integration_out("integration.dat");
-  integration_out << "#N   Simpsons           Milne          GSL        error_1         error_2"<<endl;
+  integration_out << "#N   Simpsons           Milne          GSL        error_1         error_2         (A(N)-A(2N))/A(2N)"<<endl;
   integration_out << "-------------------------------------"<<endl;
   //Milne rule should have at least 5 parts and will get more parts by adding 4i
   for (int i= 5; i<=max_intervals; i +=4)
   {
     integration_out << setw(4) << i;
     result = Simpsons_rule (i, a, b, &my_integrand);
+    double result_2 = Simpsons_rule (2.*i, a, b, &my_integrand);//get the value when N->2N
     double m=result;                   //get the value of Simpsons integration
     integration_out << " " << setprecision(12) << result;
     result = Milne_rule (i, a, b, &my_integrand);
@@ -53,7 +64,8 @@ int main()
     integration_out << " " << setprecision(12) << result;
     gsl_integration_qags (&My_function, b, a, abs_error, rel_error, 1000, work_ptr, &result, &error);
     integration_out << " " << setprecision(12) << result;
-    integration_out << " " << scientific << result-m << " " << result-n << endl;
+    integration_out << " " << scientific << result-m << " " << result-n ;//<< endl;
+    integration_out << " " << scientific <<  " " << (m - result_2)/result_2 << endl;
     //get the error of two ways
   }
   cout << "data stored in integration.dat" << endl;
@@ -65,10 +77,10 @@ int main()
 //the function to be integrated
 double my_integrand (double x)
 {
-  return  (log(2.*x)/sqrt(x));//(log (sqrt(6.*pow(x,3)))-.3*pow(x,4)+5.*exp (x));//+sin(.2*pow(x,4)+0.1)));
+  return  (log(2.*x)/sqrt(x));//(1./((1.+x)*sqrt(x)));
 }
 double My_integrand (double x, void *params)
 {
   double alpha = *(double * ) params;
-  return  (log(2.*alpha*x)/sqrt(x));//(log (alpha *sqrt(6.*pow(x,3)))-.3*pow(x,4)+5.*exp (x));//+sin(.2*pow(x,4)+0.1)));
+  return  (alpha*log(2.*x)/sqrt(x));//(1./((1.+x)*sqrt(x)*alpha));   
 }
